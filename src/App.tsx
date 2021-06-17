a3b366b44988e87ab43ec0f3ef30f7637dd7299a
jiayouzhao <jiayouzhaodaye@163.com>
2021-06-17 22:16:50 +0800

    页面路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,64 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { 
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	Redirect
+} from "react-router-dom";
 import "./App.scss";
 
 function App() {
 	return (
-		<div className="App">
-			
+		<Router>
+        
+			<div className="App">
+				<ul>
+					<li><Link to="/tags">标签</Link> </li>
+					<li><Link to="/money">记一笔</Link></li>
+					<li><Link to="/statistics">统计</Link></li>
+				</ul>
+				<Switch>
+					<Redirect exact from="/" to="/money"></Redirect>
+					<Route path="/tags"><Tags></Tags></Route>
+					<Route path="/money"><Money></Money></Route>
+					<Route path="/statistics"><Statistic></Statistic></Route>
+					<Route path="*"><NoMatch></NoMatch></Route>
+				</Switch>
+			</div>
+		</Router>
+	);
+}
+
+function NoMatch() {
+	return (
+		<div>
+            页面地址不存在
+		</div>
+	);
+}
+
+function Tags() {
+	return (
+		<div>
+            标签页
+		</div>
+	);
+}
+
+function Money() {
+	return (
+		<div>
+            记一笔
+		</div>
+	);
+
+}
+
+function Statistic() {
+	return (
+		<div>
+            统计页
 		</div>
 	);
 }
